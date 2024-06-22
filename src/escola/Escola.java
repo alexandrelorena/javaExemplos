1a327424ee1aeeb49413a58b9d73e3cc02d7c2dd
Alexandre Lorena <alexandre.lorena@gmail.com>
2024-06-22 18:30:16 -0300

    atualizações

---
+++ src/escola/Escola.java
@@ -0,0 +1,13 @@
+package escola;
+
+public class Escola {
+	public static void main(String[] args) {
+		Aluno felipe = new Aluno();
+		felipe.setNome("Felipe");
+		felipe.setIdade(8);
+		
+		System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos ");
+		//RESULTADO NO CONSOLE
+		//O aluno Felipe tem 8 anos 		
+	}
+}
