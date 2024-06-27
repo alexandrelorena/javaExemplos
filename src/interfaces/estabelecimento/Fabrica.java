de2a3338f16bd16251a87bec018161708e10fc7f
Alexandre Lorena <alexandre.lorena@gmail.com>
2024-06-26 22:33:24 -0300

    atualizações

--- src/interfaces/estabelecimento/Fabrica.java
+++ src/interfaces/estabelecimento/Fabrica.java
@@ -2,10 +2,10 @@ package interfaces.estabelecimento;
 
 import interfaces.equipamentos.copiadora.Copiadora;
 import interfaces.equipamentos.digitalizadora.Digitalizadora;
-import interfaces.equipamentos.digitalizadora.Scanner;
-import interfaces.equipamentos.impressora.Deskjet;
+// import interfaces.equipamentos.digitalizadora.Scanner;
+// import interfaces.equipamentos.impressora.Deskjet;
 import interfaces.equipamentos.impressora.Impressora;
-import interfaces.equipamentos.impressora.Laserjet;
+// import interfaces.equipamentos.impressora.Laserjet;
 import interfaces.equipamentos.multifuncional.EquipamentoMultifuncional;
 public class Fabrica {
     public static void main(String[] args) {
